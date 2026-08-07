f9a235f5d76518647fa053c1f681525e557bf523
Cleysson Santos <149117307+cleysson-santos@users.noreply.github.com>
2026-08-07 17:40:11 -0300

    membros estaticos

---
+++ src/application/Radius.java
@@ -0,0 +1,26 @@
+package application;
+
+import util.Calculator;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Radius {
+    static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter radius: ");
+        double radius = sc.nextDouble();
+
+        double circumference = Calculator.circumference(radius);
+
+        double volume = Calculator.volume(radius);
+
+        System.out.printf("Circumference: %.2f%n", circumference);
+        System.out.printf("Volume: %.2f%n", volume);
+        System.out.printf("PI value: %.2f%n", Calculator.PI);
+
+        sc.close();
+    }
+}
