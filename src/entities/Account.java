fad4f500c7cceb108d07c2aa06e772b3eb27c559
Cleysson Santos <149117307+cleysson-santos@users.noreply.github.com>
2026-08-08 18:57:05 -0300

    exercicio sobre encapsulamento (projeto cadastro banco)

---
+++ src/entities/Account.java
@@ -0,0 +1,52 @@
+package entities;
+
+public class Account {
+    private int numberAccount;
+    private String holder;
+    private double balance;
+
+    public Account(int numberAccount, String holder) {
+        this.numberAccount = numberAccount;
+        this.holder = holder;
+    }
+
+    public Account(int numberAccount, String holder, double initialDeposit) {
+        this.numberAccount = numberAccount;
+        this.holder = holder;
+        deposit(initialDeposit);
+    }
+
+    public int getNumberAccount() {
+        return numberAccount;
+    }
+
+    public String getHolder() {
+        return holder;
+    }
+
+    public void setHolder(String holder) {
+        this.holder = holder;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        balance -= amount + 5.0;
+    }
+
+    public String toString() {
+        return "Account "
+                + numberAccount
+                + ", Holder: "
+                + holder
+                + ", Balance: $"
+                + String.format("%.2f", balance);
+    }
+
+}
