5a08b8c6ccfdc5ec75023821aa66c6dacf974007
Cleysson Santos <149117307+cleysson-santos@users.noreply.github.com>
2026-08-13 20:21:09 -0300

    Exercicio Contrato trabalhador utilizando Enumeracao e Composicao

---
+++ src/EnumeracoesComposicao/Exercicio01/Application/ContratoTrabalhadorTest.java
@@ -0,0 +1,67 @@
+package EnumeracoesComposicao.Exercicio01.Application;
+
+import EnumeracoesComposicao.Exercicio01.Entities.Department;
+import EnumeracoesComposicao.Exercicio01.Entities.HourContract;
+import EnumeracoesComposicao.Exercicio01.Entities.Worker;
+import EnumeracoesComposicao.Exercicio01.enums.WorkerLevel;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContratoTrabalhadorTest {
+    static void main(String[] args) throws ParseException {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+
+        System.out.print("Enter department's name: ");
+        String departmentName = sc.nextLine();
+
+        System.out.println("Enter worder data: ");
+        System.out.print("Name: ");
+        String workerName = sc.nextLine();
+
+        System.out.print("Level: ");
+        String workerLevel = sc.nextLine();
+
+        System.out.print("Base salary: ");
+        double baseSalary = sc.nextDouble();
+
+        Worker worker = new Worker(workerName, WorkerLevel.valueOf(workerLevel), baseSalary, new Department(departmentName));
+
+        System.out.print("How many contracts to this worker? ");
+        int quantityContracts = sc.nextInt();
+
+        for (int i = 0; i < quantityContracts; i++) {
+            System.out.println("Enter contract #" + (i + 1) + " data: ");
+            System.out.print("Date (DD/MM/YYYY): ");
+            Date contractDate = simpleDateFormat.parse(sc.next());
+
+            System.out.print("Value per hour: ");
+            double valuePerHour = sc.nextDouble();
+
+            System.out.print("Duration (hours): ");
+            int hours = sc.nextInt();
+
+            System.out.println();
+
+            HourContract contract = new HourContract(contractDate, valuePerHour, hours);
+            worker.addContract(contract);
+        }
+
+        System.out.println();
+        System.out.print("Enter month and year to calculate income (MM/YYYY): ");
+        String monthAndYear = sc.next();
+        int month = Integer.parseInt(monthAndYear.substring(0, 2));
+        int year = Integer.parseInt(monthAndYear.substring(3));
+
+        System.out.println("Name: " + worker.getName());
+        System.out.println("Department: " + worker.getDepartment().getName());
+        System.out.println("Income for " + monthAndYear + ": " + String.format("%.2f",worker.income(year, month)));
+
+        sc.close();
+    }
+}
---
+++ src/EnumeracoesComposicao/Exercicio01/enums/WorkerLevel.java
@@ -0,0 +1,7 @@
+package EnumeracoesComposicao.Exercicio01.enums;
+
+public enum WorkerLevel {
+    JUNIOR,
+    MID_LEVEL,
+    SENIOR;
+}
