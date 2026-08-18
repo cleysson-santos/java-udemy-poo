f49cd26fccdf294eac039de32f6c4440e61dfa69
Cleysson Santos <149117307+cleysson-santos@users.noreply.github.com>
2026-08-18 18:20:50 -0300

    Exercicio de fixacao sobre heranca com lista

---
+++ src/HerancaEPolimorfismo/Exercicios/Application/EmployeeDatasTest.java
@@ -0,0 +1,55 @@
+package HerancaEPolimorfismo.Exercicios.Application;
+
+import HerancaEPolimorfismo.Exercicios.Entities.Employee;
+import HerancaEPolimorfismo.Exercicios.Entities.OutsourcedEmployee;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class EmployeeDatasTest {
+    static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        List<Employee> list = new ArrayList<>();
+
+        System.out.print("Enter the number of employees: ");
+        int quantity = sc.nextInt();
+
+        for (int i = 0; i < quantity; i++) {
+            System.out.println("Employee #" + (i + 1) + " data: ");
+            System.out.print("Outsourced (y/n)? ");
+            char question = sc.next().charAt(0);
+
+            System.out.print("Name: ");
+            sc.nextLine();
+            String name = sc.nextLine();
+
+            System.out.print("Hours: ");
+            Integer hours = sc.nextInt();
+
+            System.out.print("Value per hour: ");
+            Double valuePerHour = sc.nextDouble();
+
+            if(question == 'y') {
+                System.out.print("Additional charge: ");
+                double additionalCharge = sc.nextDouble();
+
+                list.add(new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge));
+            } else {
+                list.add(new Employee(name, hours, valuePerHour));
+            }
+        }
+
+        System.out.println();
+        System.out.println("PAYMENTS: ");
+        for(Employee emp : list) {
+            System.out.println(emp.getName() + " - $ " + String.format("%.2f", emp.payment()));
+        }
+
+        sc.close();
+    }
+}
