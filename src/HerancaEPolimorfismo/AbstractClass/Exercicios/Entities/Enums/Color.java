3bc5b6de87d502a39c7d8c5f37cb11b062ddaef9
Cleysson Santos <149117307+cleysson-santos@users.noreply.github.com>
2026-08-19 21:05:28 -0300

    exercicio01 utilizando classe abstrata

---
+++ src/HerancaEPolimorfismo/AbstractClass/Exercicios/Entities/Enums/Color.java
@@ -0,0 +1,7 @@
+package HerancaEPolimorfismo.AbstractClass.Exercicios.Entities.Enums;
+
+public enum Color {
+    BLACK,
+    BLUE,
+    RED;
+}
---
+++ src/HerancaEPolimorfismo/AbstractClass/Exercicios/Entities/Shape.java
@@ -0,0 +1,24 @@
+package HerancaEPolimorfismo.AbstractClass.Exercicios.Entities;
+
+import HerancaEPolimorfismo.AbstractClass.Exercicios.Entities.Enums.Color;
+
+public abstract class Shape {
+    private Color color;
+
+    public Shape() {
+    }
+
+    public Shape(Color color) {
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public abstract double area();
+}
