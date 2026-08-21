907885b35ce8fb47101e3a7851852fe2c2bf2206
Cleysson Santos <149117307+cleysson-santos@users.noreply.github.com>
2026-08-21 18:34:58 -0300

    Tratamento de excecao com try-catch e teste de StackTrace

---
+++ src/Excecoes/TryCatchTest01.java
@@ -0,0 +1,26 @@
+package Excecoes;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class TryCatchTest01 {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            String[] vect = sc.nextLine().split(" ");
+            int position = sc.nextInt();
+            System.out.println(vect[position]);
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid position!");
+        }
+        catch (InputMismatchException e) {
+            System.out.println("Input error");
+        }
+
+        System.out.println("End of program");
+
+        sc.close();
+    }
+}
